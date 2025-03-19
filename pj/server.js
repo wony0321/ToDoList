00c4a438b6fd51e43da4f4a8101db828ba435185
M6F\student <lyw1212@gmail.com>
2025-03-19 09:56:52 +0900

    todolist_꾸미기

--- pj/server.js
+++ pj/server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'app.html'));
 });
